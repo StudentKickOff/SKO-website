2d21e37e63e79277745e4176a27d0546e37b1a84
Tom Naessens <Tom.Naessens@UGent.be>
2013-07-24 21:14:53 +0200

    Started implementing the design

--- sites/all/themes/SKO2013/templates/page.tpl.php
+++ sites/all/themes/SKO2013/templates/page.tpl.php
@@ -1,215 +1,126 @@
-<?php
-
-/**
- * @file
- * Bartik's theme implementation to display a single Drupal page.
- *
- * The doctype, html, head and body tags are not in this template. Instead they
- * can be found in the html.tpl.php template normally located in the
- * modules/system directory.
- *
- * Available variables:
- *
- * General utility variables:
- * - $base_path: The base URL path of the Drupal installation. At the very
- *   least, this will always default to /.
- * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/bartik.
- * - $is_front: TRUE if the current page is the front page.
- * - $logged_in: TRUE if the user is registered and signed in.
- * - $is_admin: TRUE if the user has permission to access administration pages.
- *
- * Site identity:
- * - $front_page: The URL of the front page. Use this instead of $base_path,
- *   when linking to the front page. This includes the language domain or
- *   prefix.
- * - $logo: The path to the logo image, as defined in theme configuration.
- * - $site_name: The name of the site, empty when display has been disabled
- *   in theme settings.
- * - $site_slogan: The slogan of the site, empty when display has been disabled
- *   in theme settings.
- * - $hide_site_name: TRUE if the site name has been toggled off on the theme
- *   settings page. If hidden, the "element-invisible" class is added to make
- *   the site name visually hidden, but still accessible.
- * - $hide_site_slogan: TRUE if the site slogan has been toggled off on the
- *   theme settings page. If hidden, the "element-invisible" class is added to
- *   make the site slogan visually hidden, but still accessible.
- *
- * Navigation:
- * - $main_menu (array): An array containing the Main menu links for the
- *   site, if they have been configured.
- * - $secondary_menu (array): An array containing the Secondary menu links for
- *   the site, if they have been configured.
- * - $breadcrumb: The breadcrumb trail for the current page.
- *
- * Page content (in order of occurrence in the default page.tpl.php):
- * - $title_prefix (array): An array containing additional output populated by
- *   modules, intended to be displayed in front of the main title tag that
- *   appears in the template.
- * - $title: The page title, for use in the actual HTML content.
- * - $title_suffix (array): An array containing additional output populated by
- *   modules, intended to be displayed after the main title tag that appears in
- *   the template.
- * - $messages: HTML for status and error messages. Should be displayed
- *   prominently.
- * - $tabs (array): Tabs linking to any sub-pages beneath the current page
- *   (e.g., the view and edit tabs when displaying a node).
- * - $action_links (array): Actions local to the page, such as 'Add menu' on the
- *   menu administration interface.
- * - $feed_icons: A string of all feed icons for the current page.
- * - $node: The node object, if there is an automatically-loaded node
- *   associated with the page, and the node ID is the second argument
- *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
- *   comment/reply/12345).
- *
- * Regions:
- * - $page['header']: Items for the header region.
- * - $page['featured']: Items for the featured region.
- * - $page['highlighted']: Items for the highlighted content region.
- * - $page['help']: Dynamic help text, mostly for admin pages.
- * - $page['content']: The main content of the current page.
- * - $page['sidebar_first']: Items for the first sidebar.
- * - $page['triptych_first']: Items for the first triptych.
- * - $page['triptych_middle']: Items for the middle triptych.
- * - $page['triptych_last']: Items for the last triptych.
- * - $page['footer_firstcolumn']: Items for the first footer column.
- * - $page['footer_secondcolumn']: Items for the second footer column.
- * - $page['footer_thirdcolumn']: Items for the third footer column.
- * - $page['footer_fourthcolumn']: Items for the fourth footer column.
- * - $page['footer']: Items for the footer region.
- *
- * @see template_preprocess()
- * @see template_preprocess_page()
- * @see template_process()
- * @see bartik_process_page()
- * @see html.tpl.php
- */
-?>
-<div id="page-wrapper"><div id="page">
-
-  <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
-
-    <div id="icons">
-        <a href="https://www.facebook.com/StudentKickOff"><div class="circle"><img src="<?php print $base_path.path_to_theme(); ?>/images/icon_facebook.jpg"></img></div></a>
-        <a href="https://twitter.com/studentkickoff"><div class="circle"><img src="<?php print $base_path.path_to_theme(); ?>//images/icon_twitter.jpg"></img></div></a>
-        <a href="http://www.studentkickoff.be/contact"><div class="circle"><img src="<?php print $base_path.path_to_theme(); ?>/images/icon_mail.jpg"></img></div></a>
-    </div>
-
-          
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <strong>
-                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print strtoupper($site_name); ?></span></a>
-              </strong>
-            </div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print strtoupper($site_name);; ?></span></a>
+    <header class="site-header" id="site-header">
+        <div class="container">
+            <h1 class="site-header-logo">
+                <a href="#"><img src="<?php print $base_path.path_to_theme(); ?>/assets/images/logo-shadow.png" alt="Student Kick Off" /></a>
             </h1>
-          <?php endif; ?>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-            <?php print $site_slogan; ?>
-          </div>
-        <?php endif; ?>
-
-        <span class="schrijf_je_in">
-            SCHRIJF JE IN: 
-            ALS MEDEWERKER (coming soon) | 
-            ALS GROEP (coming soon) | 
-            <?php print l("STUDENT VILLAGE", "meewerken/studentvillage"); ?>
-        </span>
-          
-      </div> <!-- /#name-and-slogan -->
-    <?php endif; ?></div></div>
-        
-        
-    <div id="main-wrapper"><div id="main">
-
-    <?php if ($page['sidebar_first']): ?>
-      <div id="sidebar-first" class="column sidebar"><div class="section">
-        <?php print render($page['sidebar_first']); ?>
-      </div></div> <!-- /.section, /#sidebar-first -->
-    <?php endif; ?>
-
-    <div id="content" class="column"><div class="section">
-      <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php if ($tabs): ?>
-        <div class="tabs">
-          <?php print render($tabs); ?>
+            <nav>
+                <ul class="horizontal-navigation site-navigation">
+                    <!-- </li> and starting <li> have to be on the same line
+                         because display:inline-block will add whitespace otherwise -->
+                    <li class="current horizontal-navigation-item site-navigation-item">
+                        <a href="index.html" title="" class="current site-navigation-home">Home</a>
+                    </li><li class="horizontal-navigation-item site-navigation-item">
+                        <a href="line-up.html" title="" class="site-navigation-line-up">Line-up</a>
+                    </li><li class="horizontal-navigation-item site-navigation-item">
+                        <a href="student-village.html" title="" class="site-navigation-student-village">Student Village</a>
+                    </li><li class="current horizontal-navigation-item site-navigation-item">
+                        <a href="index.html" title="" class="current site-navigation-praktisch">Praktisch</a>
+                    </li><li class="horizontal-navigation-item site-navigation-item">
+                        <a href="historiek.html" title="" class="site-navigation-historiek">Historiek</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-      <?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links">
-          <?php print render($action_links); ?>
-        </ul>
-      <?php endif; ?>
-      
-        <?php if ($messages): ?>
-          <div id="messages"><div class="section clearfix">
-            <?php print $messages; ?>
-          </div></div> <!-- /.section, /#messages -->
-        <?php endif; ?>
-      
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+    </header>
 
-    </div></div> <!-- /.section, /#content -->
+    <section class="picture-background">
+        <div class="container centered" id="countdown">
+            <h1 class="hidden">Countdown</h1>
+            <div class="countdown-title">
+                Student Kick&#45;off 2013<br />
+                starting in
+            </div>
+            <span class="two-numbers">
+                <span id="countdown-days">
+                    <span class="number">2</span>
+                    <span class="number">7</span>
+                </span>
+                <span class="number-sub">dagen</span>
+            </span>
+            <span class="two-numbers">
+                <span id="countdown-hours">
+                    <span class="number">2</span>
+                    <span class="number">7</span>
+                </span>
+                <span class="number-sub">uren</span>
+            </span>
+            <span class="two-numbers">
+                <span id="countdown-minutes">
+                    <span class="number">2</span>
+                    <span class="number">7</span>
+                </span>
+                <span class="number-sub">minuten</span>
+            </span>
+            <ul class="social-icons">
+        <li class="social-icon social-icon-facebook">
+            <a href="#" title="">Facebook</a>
+        </li>
+        <li class="social-icon social-icon-twitter">
+            <a href="#" title="">Twitter</a>
+        </li>
+        <li class="social-icon social-icon-instagram">
+            <a href="#" title="">Instagram</a>
+        </li>
+    </ul>
+        </div>
+    </section>
 
-    <?php if ($page['sidebar_second']): ?>
-      <div id="sidebar-second" class="column sidebar"><div class="section">
-        <?php print render($page['sidebar_second']); ?>
-      </div></div> <!-- /.section, /#sidebar-second -->
-    <?php endif; ?>
+    <?php render($content); ?>
 
-  </div></div> <!-- /#main, /#main-wrapper -->
-
-  <?php if ($page['triptych_first'] || $page['triptych_middle'] || $page['triptych_last']): ?>
-    <div id="triptych-wrapper"><div id="triptych" class="clearfix">
-      <?php print render($page['triptych_first']); ?>
-      <?php print render($page['triptych_middle']); ?>
-      <?php print render($page['triptych_last']); ?>
-    </div></div> <!-- /#triptych, /#triptych-wrapper -->
-  <?php endif; ?>
-
-  <div id="footer-wrapper"><div class="section">
-
-    <?php if ($page['footer_firstcolumn'] || $page['footer_secondcolumn'] || $page['footer_thirdcolumn'] || $page['footer_fourthcolumn']): ?>
-      <div id="footer-columns" class="clearfix">
-        <?php print render($page['footer_firstcolumn']); ?>
-        <?php print render($page['footer_secondcolumn']); ?>
-        <?php print render($page['footer_thirdcolumn']); ?>
-        <?php print render($page['footer_fourthcolumn']); ?>
-      </div> <!-- /#footer-columns -->
-    <?php endif; ?>
-
-    <?php if ($page['footer']): ?>
-      <div id="footer" class="clearfix">
-        © 2013 Student Kick-off vzw | powered by <a href="http://www.drupal.org">Drupal</a>
-      </div> <!-- /#footer -->
-    <?php endif; ?>
-
-  </div></div> <!-- /.section, /#footer-wrapper -->
-
-</div></div> <!-- /#page, /#page-wrapper -->
+    <footer class="site-footer" id="site-footer">
+        <div class="container">
+            <div class="map"></div>
+            <div class="contact">
+                <div class="contact-logo">
+                    <img src="<?php print $base_path.path_to_theme(); ?>/assets/images/logo-shadow.png" class="footer-logo" />
+                </div>
+                <form class="contact-form">
+                    <h1>Contact</h1>
+                    <div class="input-group">
+                        <label for="input-name" class="label-icon label-icon-name">
+                            Je naam
+                        </label>
+                        <input type="text" class="input-prefixed" name="name" id="input-name" placeholder="Je naam" />
+                    </div>
+                    <div class="input-group">
+                        <label for="input-email" class="label-icon label-icon-email">
+                            Je e&#45;mailadres
+                        </label>
+                        <input type="email" class="input-prefixed" name="email" id="input-email" placeholder="Je e&#45;mailadres" />
+                    </div>
+                    <div class="input-group">
+                        <label for="input-message" class="label-icon label-icon-message">
+                            Je bericht
+                        </label>
+                        <textarea name="message" id="input-message" class="input-prefixed" placeholder="Je bericht"></textarea>
+                    </div>
+                    <div>
+                        <input type="submit" value="Verzenden" class="button" />
+                    </div>
+                </form>
+                <div class="contact-information">
+                    <ul class="social-icons">
+                        <li class="social-icon social-icon-facebook">
+                            <a href="#" title="">Facebook</a>
+                        </li>
+                        <li class="social-icon social-icon-twitter">
+                            <a href="#" title="">Twitter</a>
+                        </li>
+                        <li class="social-icon social-icon-instagram">
+                            <a href="#" title="">Instagram</a>
+                        </li>
+                    </ul>
+                    <h3>Student Kick&#45;off</h3>
+                    <p>
+                        Adres regel 01<br />
+                        Adres regel 02
+                    </p>
+                    <p>
+                        +32 479 60 87 70
+                    </p>
+                </div>
+            </div>
+            <p class="copyright">
+                &copy;2005&#45;2013 Student Kick&#45;off vzw
+        </div>
+    </footer>
