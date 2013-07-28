b7486d3627ef9cb6256f71b8a7a69e232aa2927f
Tom Naessens <Tom.Naessens@UGent.be>
2013-07-28 12:50:39 +0200

    Fixed the z-idexes for the admin menu overlay

--- sites/all/themes/SKO2013/assets/js/simpleSlider.js
+++ sites/all/themes/SKO2013/assets/js/simpleSlider.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#slider').simpleSlider({
         width: 630,
         height: 430,
-        zIndex: 500, // z-index offset
+        zIndex: 499, // z-index offset
         effect: 'horizontal',
         navigation: false, // show the previous and next buttons
         bullets: false, // show the bullet navigation
