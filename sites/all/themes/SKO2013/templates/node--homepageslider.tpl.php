4b982a545e19acf290e2b4e52b4ab1c9d3544c6b
Tom Naessens <Tom.Naessens@UGent.be>
2013-07-29 19:35:06 +0200

    Added optional clickable link to homepageslider (closes #42)

--- sites/all/themes/SKO2013/templates/node--homepageslider.tpl.php
+++ sites/all/themes/SKO2013/templates/node--homepageslider.tpl.php
@@ -1,5 +1,13 @@
-<?php print render($content['field_afbeelding']); ?>
+<?php if (!empty($content['field_link'])) : ?>
+    <a href="<?php print render($content['field_link']); ?>" target="_blank" >
+<?php endif; ?>
 
-<?php if ($node->field_toon_titel['und'][0]['value'] == '1'): ?>
-    <span class="slider-item-caption"><?php print $title; ?></span>
+    <?php print render($content['field_afbeelding']); ?>
+
+    <?php if ($node->field_toon_titel['und'][0]['value'] == '1'): ?>
+        <span class="slider-item-caption"><?php print $title; ?></span>
+    <?php endif; ?>
+
+<?php if (!empty($content['field_link'])) : ?>
+    </a>
 <?php endif; ?>
