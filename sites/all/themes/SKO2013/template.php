1518118681fc26ba3b6283dd9fa3a346e8c7b2de
Tom Naessens <Tom.Naessens@UGent.be>
2013-07-29 16:40:27 +0200

    Added viewport (updates #1)

--- sites/all/themes/SKO2013/template.php
+++ sites/all/themes/SKO2013/template.php
@@ -1,5 +1,17 @@
 <?php
 
+// Responsiveness
+function SKO2013_preprocess_html(&$vars) {
+    $viewport = array(
+        '#tag' => 'meta',
+        '#attributes' => array(
+            'name' => 'viewport',
+            'content' => 'width=device-width, initial-scale=1, maximum-scale=1',
+        ),
+    );
+    drupal_add_html_head($viewport, 'viewport');
+}
+
 // Fancy breadcrumbs
 function SKO2013_breadcrumb($variables) {
     $breadcrumb = $variables['breadcrumb'];
