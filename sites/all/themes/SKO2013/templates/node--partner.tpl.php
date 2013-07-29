21cefa06d2b6b79eaeb7017ba347d9f4a2812f2c
Tom Naessens <Tom.Naessens@UGent.be>
2013-07-29 23:21:32 +0200

    Target blank for partner (closes #47)

--- sites/all/themes/SKO2013/templates/node--partner.tpl.php
+++ sites/all/themes/SKO2013/templates/node--partner.tpl.php
@@ -1,7 +1,7 @@
 <?php if($id % 3 == 1) : ?>
 <li>
 <?php endif; ?>
-    <a href="<?php print $content['field_link'][0]['#markup']; ?>" alt="<?php print $title; ?>">
+    <a href="<?php print $content['field_link'][0]['#markup']; ?>" alt="<?php print $title; ?>" target="_blank">
         <?php print render($content['field_logo']); ?>
     </a>
 <?php if($id % 3 == 3) : ?>
