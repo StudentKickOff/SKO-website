6c92ce44bf066dcceadba9e07fa3242b2f45a0ca
Tom Naessens <Tom.Naessens@UGent.be>
2013-08-03 17:18:58 +0200

    Nice code is nice <3 (closes #81 #82)

--- sites/all/themes/SKO2013/templates/page--front.tpl.php
+++ sites/all/themes/SKO2013/templates/page--front.tpl.php
@@ -1,191 +1,35 @@
-    <header class="site-header" id="site-header">
-        <div class="container">
-            <h1 class="site-header-logo">
-                <a href="#"><img src="<?php print $base_path.path_to_theme(); ?>/assets/images/logo-shadow.png" alt="Student Kick Off" /></a>
-            </h1>
-            <nav>
-                <ul class="horizontal-navigation site-navigation">
-                     <!--</li> and starting <li> have to be on the same line
-                         because display:inline-block will add whitespace otherwise-->
-                    <li class="current horizontal-navigation-item site-navigation-item">
-                         <?php print l('Home', 'frontpage', array('attributes' => array('class' => array('current site-navigation-home')))); ?>
-                    </li><li class="horizontal-navigation-item site-navigation-item">
-                         <?php print l('Line-up', 'line-up', array('attributes' => array('class' => array('current site-navigation-line-up')))); ?>
-                    </li><li class="horizontal-navigation-item site-navigation-item">
-                         <?php print l('Student village', 'studentvillage', array('attributes' => array('class' => array('current site-navigation-student-village')))); ?>
-                    </li><li class="current horizontal-navigation-item site-navigation-item">
-                         <?php print l('Info', 'info', array('attributes' => array('class' => array('current site-navigation-praktisch')))); ?>
-                    </li><li class="horizontal-navigation-item site-navigation-item">
-                         <?php print l('Historiek', 'historiek', array('attributes' => array('class' => array('current site-navigation-historiek')))); ?>
-                    </li><li class="horizontal-navigation-item site-navigation-item" id="nav-toggle"><a href="#">&#9776;</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+<?php include('includes/header.inc'); ?>
 
-    <section class="picture-background">
-        <div class="container centered" id="countdown">
-            <h1 class="hidden">Countdown</h1>
-            <div class="countdown-title">
-                Student Kick&#45;off 2013<br />
-                starting in
+<?php include('includes/subheader-large.inc'); ?>
+
+<?php include('includes/section-praktisch.inc'); ?>
+
+<section class="pattern-background">
+    <div class="container has-sidebar">
+        <div class="main">
+            <h1>What&rsquo;s hot!?</h1>
+            <div class="slider-container">
+                <div id="slider">
+                    <?php print render($page['hot']); ?>
+                </div>
+                <a href="#" class="slider-previous-arrow">Previous</a>
+                <a href="#" class="slider-next-arrow">Next</a>
             </div>
-            <span class="two-numbers">
-                <span id="countdown-days">
-                    <span class="number">2</span>
-                    <span class="number">7</span>
-                </span>
-                <span class="number-sub">dagen</span>
-            </span>
-            <span class="two-numbers">
-                <span id="countdown-hours">
-                    <span class="number">2</span>
-                    <span class="number">7</span>
-                </span>
-                <span class="number-sub">uren</span>
-            </span>
-            <span class="two-numbers">
-                <span id="countdown-minutes">
-                    <span class="number">2</span>
-                    <span class="number">7</span>
-                </span>
-                <span class="number-sub">minuten</span>
-            </span>
         </div>
-    </section>
-
-    <section class="white-background">
-        <div class="top-social-icons">
-            <ul class="social-icons">
-                <li class="social-icon social-icon-facebook">
-                    <a href="http://www.facebook.com/StudentKickOff" title="" target="_blank">Facebook</a>
-                </li>
-                <li class="social-icon social-icon-twitter">
-                    <a href="http://twitter.com/studentkickoff" title="" target="_blank">Twitter</a>
-                </li>
-                <li class="social-icon social-icon-instagram">
-                    <a href="#" title="">Instagram</a>
-                </li>
-            </ul>
-        </div>
-        <div class="container centered">
-            <h1 class="hidden">Praktisch</h1>
-            <ul class="feature-list">
-                <li class="feature-list-item">
-                    <img class="feature-list-item-image" src="<?php print $base_path.path_to_theme(); ?>/assets/images/feature-waar.png" />
-                    <h2>Waar?</h2>
-                    <p>
-                        Sint&ndash;Pietersplein<br />
-                        9000 Gent
-                    </p>
-                </li>
-                <li class="feature-list-item">
-                    <img class="feature-list-item-image" src="<?php print $base_path.path_to_theme(); ?>/assets/images/feature-wanneer.png" />
-                    <h2>Wanneer?</h2>
-                    <p>
-                        Woensdag 25 september
-                    </p>
-                </li>
-                <li class="feature-list-item">
-                    <img class="feature-list-item-image" src="<?php print $base_path.path_to_theme(); ?>/assets/images/feature-hoe-laat.png" />
-                    <h2>Hoe laat?</h2>
-                    <p>
-                        Vanaf 12 uur tot in de vroege uurtjes
-                    </p>
-                </li>
-            </ul>
-            <?php print l('Meer praktische info', 'info', array('attributes' => array('class' => array('button')))); ?>
-        </div>
-    </section>
-
-    <section class="pattern-background">
-        <div class="container has-sidebar">
-            <div class="main">
-                <h1>What&rsquo;s hot!?</h1>
-                <div class="slider-container">
-                    <div id="slider">
-                        <?php print render($page['hot']); ?>
-                    </div>
-                    <a href="#" class="slider-previous-arrow">Previous</a>
-                    <a href="#" class="slider-next-arrow">Next</a>
-                </div>
+        <aside class="sidebar-closer">
+            <h2>Onze partners</h2>
+            <div id="partner-slider">
+                <?php print render($page['partners']); ?>
             </div>
-            <aside class="sidebar-closer">
-                <h2>Onze partners</h2>
-                <div id="partner-slider">
-                    <?php print render($page['partners']); ?>
-                </div>
-            </aside>
-        </div>
-    </section>
+        </aside>
+    </div>
+</section>
 
-    <section>
-        <div class="container">
-            <?php print render($page['content']); ?>
-        </div>
-    </section>
+<section>
+    <div class="container">
+        <?php print render($page['content']); ?>
+    </div>
+</section>
 
-    <footer class="site-footer" id="site-footer">
-        <div class="top-social-icons">
-            <ul class="social-icons">
-                <li class="social-icon social-icon-facebook">
-                    <a href="http://www.facebook.com/StudentKickOff" title="" target="_blank">Facebook</a>
-                </li>
-                <li class="social-icon social-icon-twitter">
-                    <a href="http://twitter.com/studentkickoff" title="" target="_blank">Twitter</a>
-                </li>
-                <li class="social-icon social-icon-instagram">
-                    <a href="#" title="">Instagram</a>
-                </li>
-            </ul>
-        </div>
-        <div class=" container">
-            <div class="contact">
-                <div class="contact-logo">
-                    <img src="<?php print $base_path.path_to_theme(); ?>/assets/images/logo-shadow.png" class="footer-logo" />
-                </div>
+<?php include('includes/footer.inc'); ?>
 
-                <div class="two-columns">
-                    <div class="column">
-                        <?php print render($page['footer']); ?>
-                    </div>
-
-                    <div class="column">
-                        <div class="contact-information">
-                     
-                            <h2>Meewerken?</h2>
-                            <?php print l('Individueel', 'meewerken/inschrijven', array('attributes' => array('class' => array('button')))); ?>
-                            <?php print l('Groep', 'meewerken/groepen', array('attributes' => array('class' => array('button')))); ?>
-
-                            <p></p>
-
-                            <h3>Standhouder Student Village?</h3>
-                            <?php print l('Schrijf je nu in', 'meewerken/studentvillage', array('attributes' => array('class' => array('button button-red')))); ?>
-
-                            <p></p>
-
-                            <h3>Pers?</h3>
-                            <?php print l('Schrijf je nu in', 'pers', array('attributes' => array('class' => array('button button-yellow')))); ?>
-
-                            <p></p>
-
-                            <h3>Student Kick&#45;off vzw</h3>
-                            <p>
-                                <b>Maatschappelijke zetel</b><br />
-                                Botermarkt 1<br />
-                                9000 Gent<br />
-                                BTW BE 0886.847.343
-                            </p>
-                            <p>
-                                <b>Postadres</b><br />
-                                Blekerijstraat 75 bus 3<br />
-                                9000 Gent
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <p class="copyright">
-                &copy;2005&#45;2013 Student Kick&#45;off vzw
-        </div>
-    </footer>
--- sites/all/themes/SKO2013/templates/page--studentvillage.tpl.php
+++ sites/all/themes/SKO2013/templates/page--studentvillage.tpl.php
@@ -1,142 +1,37 @@
-    <header class="site-header" id="site-header">
-        <div class="container">
-            <h1 class="site-header-logo">
-                <a href="#"><img src="<?php print $base_path.path_to_theme(); ?>/assets/images/logo-shadow.png" alt="Student Kick Off" /></a>
-            </h1>
-            <nav>
-                <ul class="horizontal-navigation site-navigation">
-                     <!--</li> and starting <li> have to be on the same line
-                         because display:inline-block will add whitespace otherwise-->
-                    <li class="current horizontal-navigation-item site-navigation-item">
-                         <?php print l('Home', 'frontpage', array('attributes' => array('class' => array('current site-navigation-home')))); ?>
-                    </li><li class="horizontal-navigation-item site-navigation-item">
-                         <?php print l('Line-up', 'line-up', array('attributes' => array('class' => array('current site-navigation-line-up')))); ?>
-                    </li><li class="horizontal-navigation-item site-navigation-item">
-                         <?php print l('Student village', 'studentvillage', array('attributes' => array('class' => array('current site-navigation-student-village')))); ?>
-                    </li><li class="current horizontal-navigation-item site-navigation-item">
-                         <?php print l('Info', 'info', array('attributes' => array('class' => array('current site-navigation-praktisch')))); ?>
-                    </li><li class="horizontal-navigation-item site-navigation-item">
-                         <?php print l('Historiek', 'historiek', array('attributes' => array('class' => array('current site-navigation-historiek')))); ?>
-                    </li><li class="horizontal-navigation-item site-navigation-item" id="nav-toggle"><a href="#">&#9776;</a></li>                
-                </ul>
-            </nav>
-        </div>
-    </header>
+<?php include('includes/header.inc'); ?>
 
-    <section class="picture-background">
-        <div class="container centered" id="countdown">
-            <h1 class="hidden">Countdown</h1>
-            <div class="countdown-title">
-                Student Kick&#45;off 2013<br />
+<?php include('includes/subheader-small.inc'); ?>
+
+<section>
+    <?php include('includes/social-icons.inc'); ?>
+    <div class="container"  id="scrollTo">
+        <p class="breadcrumb">
+            <?php print $breadcrumb ?>
+        </p>
+        <?php if ($messages): ?>
+            <div id="messages">
+                <div class="section clearfix">
+                    <?php print $messages; ?>
+                </div>
+            </div> <!-- /.section, /#messages -->
+        <?php endif; ?>
+
+        <div class="two-columns">
+            <div class="column white-background column-no-padding">
+                <div class="detail-full-width-image">
+                    <img src="<?php print $base_path . path_to_theme(); ?>/assets/images/studentvillage-header.jpg" />
+                </div>
+                <?php
+                $nid = 82;
+                print drupal_render(node_view(node_load($nid)));
+                ?>
+            </div>
+            <div class="student-village-container column white-background column-no-padding">
+                <h1>Programmatie</h1>
+                <?php print render($page['content']); ?>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section>
-        <div class="top-social-icons">
-            <ul class="social-icons">
-                <li class="social-icon social-icon-facebook">
-                    <a href="http://www.facebook.com/StudentKickOff" title="" target="_blank">Facebook</a>
-                </li>
-                <li class="social-icon social-icon-twitter">
-                    <a href="http://twitter.com/studentkickoff" title="" target="_blank">Twitter</a>
-                </li>
-                <li class="social-icon social-icon-instagram">
-                    <a href="#" title="">Instagram</a>
-                </li>
-            </ul>
-        </div>
-        <div class="container"  id="scrollTo">
-            <p class="breadcrumb">
-                <?php print $breadcrumb ?>
-            </p>
-            <?php if ($messages): ?>
-                <div id="messages">
-                    <div class="section clearfix">
-                        <?php print $messages; ?>
-                    </div>
-                </div> <!-- /.section, /#messages -->
-            <?php endif; ?>
-                    
-            <div class="two-columns">
-                <div class="column white-background column-no-padding">
-                    <div class="detail-full-width-image">
-                        <img src="<?php print $base_path.path_to_theme(); ?>/assets/images/studentvillage-header.jpg" />
-                    </div>
-                    <?php
-                        $nid = 82;
-                        print drupal_render(node_view(node_load($nid)));
-                    ?>
-                </div>
-                <div class="student-village-container column white-background column-no-padding">
-                    <h1>Programmatie</h1>
-                    <?php print render($page['content']); ?>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <footer class="site-footer" id="site-footer">
-        <div class="top-social-icons">
-            <ul class="social-icons">
-                <li class="social-icon social-icon-facebook">
-                    <a href="http://www.facebook.com/StudentKickOff" title="" target="_blank">Facebook</a>
-                </li>
-                <li class="social-icon social-icon-twitter">
-                    <a href="http://twitter.com/studentkickoff" title="" target="_blank">Twitter</a>
-                </li>
-                <li class="social-icon social-icon-instagram">
-                    <a href="#" title="">Instagram</a>
-                </li>
-            </ul>
-        </div>
-        <div class=" container">
-            <div class="contact">
-                <div class="contact-logo">
-                    <img src="<?php print $base_path.path_to_theme(); ?>/assets/images/logo-shadow.png" class="footer-logo" />
-                </div>
-                
-                <div class="two-columns">
-                    <div class="column">
-                        <?php print render($page['footer']); ?>
-                    </div>
-                    
-                    <div class="column">
-                        <div class="contact-information">
-
-                            <h2>Meewerken?</h2>                    
-                            <?php print l('Individueel', 'meewerken/inschrijven', array('attributes' => array('class' => array('button')))); ?>
-                            <?php print l('Groep', 'meewerken/groepen', array('attributes' => array('class' => array('button')))); ?>
-
-                            <p></p>
-
-                            <h3>Standhouder Student Village?</h3>
-                            <?php print l('Schrijf je nu in', 'meewerken/studentvillage', array('attributes' => array('class' => array('button button-red')))); ?>
-
-                            <p></p> 
-
-                            <h3>Pers?</h3>
-                            <?php print l('Schrijf je nu in', 'pers', array('attributes' => array('class' => array('button button-yellow')))); ?>
-
-                            <p></p>
-
-                            <h3>Student Kick&#45;off vzw</h3>
-                            <p>
-                                <b>Maatschappelijke zetel</b><br />
-                                Botermarkt 1<br />
-                                9000 Gent<br />
-                                BTW BE 0886.847.343
-                            </p>
-                            <p>
-                                <b>Postadres</b><br />
-                                Blekerijstraat 75 bus 3<br />
-                                9000 Gent
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <p class="copyright">
-                &copy;2005&#45;2013 Student Kick&#45;off vzw
-        </div>
-    </footer>
+<?php include('includes/footer.inc'); ?>
