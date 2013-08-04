ea6ec601065f2ba8139bedbdb8a801e6533fea67
Tom Naessens <Tom.Naessens@UGent.be>
2013-08-04 16:42:00 +0200

    Added a special node, containing the partners praktisch view (closes #63)

---
+++ sites/all/themes/SKO2013/templates/node--86.tpl.php
@@ -0,0 +1,9 @@
+<article class="white-background detail-full-item history-item">
+    <a href="#" class="history-item-toggle"><?php print $title; ?></a>
+    <div class="detail-container">
+        <header>
+            <h1 class="no-margin no-padding"><?php print $title; ?></h1>
+        </header>
+        <?php print views_embed_view('partnerslider','praktisch_partners', $node->nid); ?>
+    </div>
+</article>
