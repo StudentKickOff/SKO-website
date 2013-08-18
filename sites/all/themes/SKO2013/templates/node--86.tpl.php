3c227d2134d8d10ce33572ba889b2d6a4220bf5b
Tom Naessens <Tom.Naessens@UGent.be>
2013-08-18 14:20:33 +0200

    Fixed partnershizzle

--- sites/all/themes/SKO2013/templates/node--86.tpl.php
+++ sites/all/themes/SKO2013/templates/node--86.tpl.php
@@ -1,9 +1,17 @@
 <article class="white-background detail-full-item history-item">
     <a href="#" class="history-item-toggle"><?php print $title; ?></a>
     <div class="detail-container">
+        <?php if(count(views_get_view_result('partnerslider','praktisch_partners_presenting', $node->nid))) : ?>
         <header>
-            <h1 class="no-margin no-padding"><?php print $title; ?></h1>
+            <h2 class="no-margin no-padding">Presenting partners</h2>
         </header>
-        <?php print views_embed_view('partnerslider','praktisch_partners', $node->nid); ?>
+        <?php print views_embed_view('partnerslider','praktisch_partners_presenting', $node->nid); ?>
+
+        <header>
+            <h2 class="no-margin no-padding">Partners</h2>
+        </header>
+        <?php endif; ?>
+
+        <?php print views_embed_view('partnerslider','praktisch_partners_normaal', $node->nid); ?>
     </div>
 </article>
--- sites/all/themes/SKO2013/templates/node--partner.tpl.php
+++ sites/all/themes/SKO2013/templates/node--partner.tpl.php
@@ -1,9 +1,9 @@
-<?php if($id % 3 == 1) : ?>
+<?php if($id % 3 == 2) : ?>
 <li>
 <?php endif; ?>
     <a href="<?php print $content['field_link'][0]['#markup']; ?>" alt="<?php print $title; ?>" target="_blank">
         <?php print render($content['field_logo']); ?>
     </a>
-<?php if($id % 3 == 3) : ?>
+<?php if($id % 3 == 1) : ?>
 </li>
 <?php endif; ?>
