f26deb97ed0fccb325512984e1d52e170ffe2202
Tom Naessens <siloxtom@gmail.com>
2013-08-26 12:47:12 +0200

    Fully fixed partners

--- sites/all/themes/SKO2013/templates/node--partner.tpl.php
+++ sites/all/themes/SKO2013/templates/node--partner.tpl.php
@@ -1,9 +1,9 @@
-<?php if($id % 3 == 2) : ?>
+<?php if($is_front && $id % 3 == 2) : ?>
 <li>
 <?php endif; ?>
     <a href="<?php print $content['field_link'][0]['#markup']; ?>" alt="<?php print $title; ?>" target="_blank">
         <?php print render($content['field_logo']); ?>
     </a>
-<?php if($id % 3 == 1) : ?>
+<?php if($is_front && $id % 3 == 1) : ?>
 </li>
 <?php endif; ?>
