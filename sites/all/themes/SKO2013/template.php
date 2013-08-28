03ee49ec70794d7da697a84b448a67c9851aac49
Tom Naessens <siloxtom@gmail.com>
2013-08-28 23:00:58 +0200

    Updated template includes

--- sites/all/themes/SKO2013/template.php
+++ sites/all/themes/SKO2013/template.php
@@ -3,5 +3,6 @@ require('includes/template_reset.inc');
 require('includes/template_preprocess.inc');
 require('includes/template_breadcrumbs.inc');
 require('includes/template_pager.inc');
+require('includes/template_webform_form_alter.inc');
 require('includes/template_webform_fancycontactform.inc');
 
