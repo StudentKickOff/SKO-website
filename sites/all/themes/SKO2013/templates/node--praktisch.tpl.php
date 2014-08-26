7b1d127486582c94d3aeed9691c9479c23257280
Tom Naessens <Tom.Naessens@UGent.be>
2014-08-26 19:46:57 +0200

    Rework praktisch page

--- sites/all/themes/SKO2013/templates/node--praktisch.tpl.php
+++ sites/all/themes/SKO2013/templates/node--praktisch.tpl.php
@@ -1,9 +1,26 @@
-<article class="white-background detail-full-item history-item">
-    <a href="#" class="history-item-toggle"><?php print $title; ?></a>
-    <div class="detail-container">
-        <?php print render($content['field_uitleg']); ?>
-        <?php if(!empty($content['field_afbeelding'])) : ?>
-             <?php print render($content['field_afbeelding']); ?>
-        <?php endif; ?>
-    </div>
-</article>
+<?php if ($teaser) : ?>
+    <article>
+        <div class="grid-item-image">
+            <div class="grid-item-image-overlay">
+                <a href="<?php print $node_url ?>" class="button button-white">Lees meer</a>
+            </div>
+            <?php if(!empty($content['field_afbeelding'])) : ?>
+                 <?php print render($content['field_afbeelding']); ?>
+            <?php endif; ?>
+        </div>
+        <div class="grid-item-content">
+            <a href="<?php print $node_url ?>">
+                <header>
+                    <?php print $title ?>
+                </header>
+            </a>
+        </div>
+    </article>
+<?php else : ?>
+    <h1><?php print $title; ?></h1>
+    <article class="white-background">
+        <div class="detail-container">
+            <?php print render($content['field_uitleg']); ?>
+        </div>
+    </article>
+<?php endif; ?>
